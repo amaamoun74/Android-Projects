63821def4ef7af6e8e14b36fa5ab945f83c1acb9
Ahmed Maamoun <amaamoun74@gmail.com>
2021-12-02 19:59:45 +0200

    second commit splash & welcome screen was added

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView appName, slogan;
+     private static final int splashDuration= 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.down_animation);
+
+        logo= findViewById(R.id.logoImageView);
+        appName= findViewById(R.id.appName_TextView);
+        slogan= findViewById(R.id.slogan_TextView2);
+
+        logo.setAnimation(topAnim);
+        appName.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,StartingApp.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashDuration);
     }
 }
