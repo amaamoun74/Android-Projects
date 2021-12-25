f9e824690e9311895232402f3b9c92e35b80543e
mennaa132 <mennaa.ashraf132@gmail.com>
2021-12-25 17:20:12 +0200

    6th commit by menna

--- app/src/main/java/UI/userProfile.java
+++ app/src/main/java/UI/userProfile.java
@@ -1,8 +1,10 @@
-package com.example.healthapp;
+package UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.healthapp.R;
 
 public class userProfile extends AppCompatActivity {
 
