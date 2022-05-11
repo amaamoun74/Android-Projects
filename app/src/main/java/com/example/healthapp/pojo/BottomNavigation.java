3cf72470becb09ad04c8f3e25bfb115463c84303
mennaa132 <mennaa.ashraf132@gmail.com>
2022-05-11 20:22:08 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/res/layout/activity_log_in.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/healthapp/pojo/BottomNavigation.java
+++ app/src/main/java/com/example/healthapp/pojo/BottomNavigation.java
@@ -32,11 +32,11 @@ public class BottomNavigation extends AppCompatActivity {
         bottomNav = findViewById(R.id.bottomNav);
         //add menu items to bottomNav
         bottomNav.add(new MeowBottomNavigation.Model(1, R.drawable.ic_baseline_person_24));
-        bottomNav.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_coronavirus_24));
+        bottomNav.add(new MeowBottomNavigation.Model(2, R.drawable.exposure_notif));
         bottomNav.add(new MeowBottomNavigation.Model(3, R.drawable.ic_home_black_24dp));
         bottomNav.add(new MeowBottomNavigation.Model(4, R.drawable.ic_baseline_more_vert_24));
         bottomNav.add(new MeowBottomNavigation.Model(5, R.drawable.qr));
-        bottomNav.add(new MeowBottomNavigation.Model(6, R.drawable.ic_baseline_login_24));
+        bottomNav.add(new MeowBottomNavigation.Model(6, R.drawable.ic_baseline_coronavirus_24));
 
         //set bottomNav on show listener
         bottomNav.setOnShowListener(new MeowBottomNavigation.ShowListener() {
@@ -54,13 +54,13 @@ public class BottomNavigation extends AppCompatActivity {
                 } else if (item.getId() == 3) {
                     fragment = new HomeFragment2();
                 } else if (item.getId() == 4) {
-                    fragment = new modelQues();
+                    fragment = new MoreOptionFragment();
                     layout.setVisibility(View.INVISIBLE);
                 } else if (item.getId() == 5) {
                     fragment = new QRScanningFragment();
                     layout.setVisibility(View.INVISIBLE);
                 } else if (item.getId() == 6) {
-                    fragment = new MoreOptionFragment();
+                    fragment = new modelQues();
                     layout.setVisibility(View.INVISIBLE);
                 }
                 loadFragment(fragment);
