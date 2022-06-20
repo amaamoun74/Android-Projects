52e789b643e76746cbcd031e8f0a5bd31d2d2699
Ahmed Maamoun <amaamoun74@gmail.com>
2022-06-20 21:41:37 +0200

    commit

--- app/src/main/java/com/example/healthapp/modelQues.java
+++ app/src/main/java/com/example/healthapp/modelQues.java
@@ -1,64 +1,120 @@
 package com.example.healthapp;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link modelQues#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
 public class modelQues extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public modelQues() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment modelQues.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static modelQues newInstance(String param1, String param2) {
-        modelQues fragment = new modelQues();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    SwitchCompat bluetoothSwitch;
+    boolean switchBtn = true;
+    BluetoothAdapter bluetoothAdapter;
+    ListView scanListView;
+    ArrayList<String> arrayList = new ArrayList<String>();
+    ArrayAdapter<String> arrayAdapter;
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                @SuppressLint("MissingPermission") String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d("tag",deviceHardwareAddress.toString());
+                arrayList.add(deviceHardwareAddress);
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_model_ques, container, false);
+        View view= inflater.inflate(R.layout.fragment_model_ques, container, false);
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        requireActivity().registerReceiver(receiver,filter);
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_2,arrayList);
+        scanListView.setAdapter(arrayAdapter);
+
+        bluetoothSwitch = view.findViewById(R.id.bleSwitch);
+        bluetoothSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switchBtn= false)
+                {
+                    switchBtn= true;
+                    bluetoothSwitch.setChecked(true);
+                    enableBluetooth();
+                }
+                else if (switchBtn= true){
+                    switchBtn= false;
+                    bluetoothSwitch.setChecked(false);
+                    enableBluetooth();
+                }
+            }
+        });
+        return view;
     }
+    @SuppressLint("MissingPermission")
+    void enableBluetooth() {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getActivity(), "ble_not_supported", Toast.LENGTH_SHORT).show();
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            // Register for broadcasts when a device is discovered.
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            requireActivity().registerReceiver(receiver, filter);
+            discoverability();
+        }
+        if(bluetoothAdapter.isEnabled()){
+            Log.d("TAG", "enableDisableBT: disabling BT.");
+            bluetoothAdapter.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            requireActivity().registerReceiver(receiver, BTIntent);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    void discoverability(){
+        int requestCode = 1;
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 24*60*60);//discoverable for 24h
+        startActivityForResult(discoverableIntent, requestCode);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        requireActivity().unregisterReceiver(receiver);
+    }
+
 }
